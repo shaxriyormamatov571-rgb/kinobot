e4079cc3431cd15b550ee370c58202b91e069056
shaxriyormamatov571-rgb <shaxriyormamatov571@gmail.com>
2026-07-14 14:25:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print("Bot ishga tayyor")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Assalomu alaykum! 🎬\nBot ishlayapti."
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot ishga tushdi...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
